77678c6ca3e1576b4361fabe6bc6f19dcf457c19
floki <flokitheprogrammer@gmail.com>
2021-11-14 18:44:34 +0500

    ran code prettier

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 //digits.innerText = 888.88888;
 
-//test
+// let buttons = document.querySelectorAll(".calc-button");
+// let clicks = [];
+// for (const button of buttons) {
+//   button.addEventListener("click", () => {
+//     clicks.push(button.id);
+//     console.log(button.id)
+//   });
+// }
