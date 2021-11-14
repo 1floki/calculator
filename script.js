fd1b72bf47f0a709527d26b3a948b2402a7795a4
floki <flokitheprogrammer@gmail.com>
2021-11-15 02:25:36 +0500

    buttons update display when pressed

--- script.js
+++ script.js
@@ -12,6 +12,39 @@
 let buttons = document.querySelectorAll(".calc-button");
 buttons.forEach((btn) => {
   btn.addEventListener("click", function takeButtonId() {
-    console.log(btn.id);
+    buttonClicked(btn.id);
   });
 });
+
+let firstNumber = "";
+let operator = "";
+let secondNumber = "";
+
+function buttonClicked(button) {
+  if (isNaN(button)) {
+    operatorPressed(button);
+  } else {
+    if (operator.length == 0 && firstNumber.length <= 7) {
+      firstNumberPressed(button);
+    } else if (operator.length == 1 && secondNumber.length <= 7) {
+      secondNumberPressed(button);
+    }
+  }
+}
+
+function operatorPressed(button) {
+  if (firstNumber.length != 0) {
+    operator = button;
+    console.log(operator);
+  }
+}
+
+function firstNumberPressed(button) {
+  firstNumber += button;
+  document.getElementById("digits").innerText = firstNumber;
+}
+
+function secondNumberPressed(button) {
+  secondNumber += button;
+  document.getElementById("digits").innerText = secondNumber;
+}
